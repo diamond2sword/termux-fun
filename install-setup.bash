98155afb11f4c41ef57b85be860083261ba68344
diamond2sword <diamond2sword@gmail.com>
2024-06-04 14:56:05 +0800

    update project

--- install-setup.bash
+++ install-setup.bash
@@ -12,8 +12,8 @@ main () {
 			echo -en "SSH Key Passphrase:"
 			read -r ssh_key_passphrase
    		fi
-  		echo "$github_personal_token" > "$HOME/github-personal-token.txt"
-		echo "$ssh_key_passphrase" > "$HOME/ssh-key-passphrase.txt"
+  		echo -n "$github_personal_token" > "$HOME/github-personal-token.txt"
+		echo -n "$ssh_key_passphrase" > "$HOME/ssh-key-passphrase.txt"
 	}
 	yes | {
 		apt update
@@ -28,7 +28,8 @@ main () {
 	 
 		#neovim
 		apt install neovim
-		sh -c 'curl -fLo ${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+		local plug_vim_path="${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim"
+		force_move_file_with_cmd f "$plug_vim_path" "curl -fLo '$plug_vim_path' --create-dirs 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'"
 		apt install fzf
        	apt install tree
        	apt install shellcheck
@@ -39,7 +40,6 @@ main () {
 	mkdir -p ~/.config/nvim/lua/lsp
 	mkdir -p ~/.config/nvim/after/syntax/sh
 	echo "$INIT_VIM" > ~/.config/nvim/init.vim
-	echo "$INIT_LUA" > ~/.config/nvim/lua/lsp/init.lua
 	echo "$VIM_SH_HEREDOC_HIGHLIGHTING" > ~/.config/nvim/after/syntax/sh/heredoc-sh.vim
 	
 	{
@@ -53,10 +53,10 @@ main () {
 			yes | { 
 				apt install expect
 			}
-			do_coc_install coc-json
-			do_coc_install coc-git
-			do_coc_install coc-sh
-			do_coc_install coc-clangd
+			coc_extension_list=("coc-json" "coc-git" "coc-sh" "coc-clangd")
+			for coc_extension in "${coc_extension_list[@]}"; do
+				force_coc_install "$coc_extension"
+			done
 			nvim +'PlugClean --sync' +qa
 		}
 	}
@@ -69,11 +69,11 @@ main () {
 		#kotlin lsp
 		apt install bat
 		apt install unzip
-		if [ -f ~/lsp/kotlin/server/bin/kotlin-language-server ]; then
+		if [ -f "$HOME/lsp/kotlin/server/bin/kotlin-language-server" ]; then
 			#escape downloading if kotlin lsp exists
 			exit
 		fi
-		curl -LJO https://github.com/fwcd/kotlin-language-server/releases/download/1.3.7/server.zip
+		force_move_file_with_cmd f "$HOME/server.zip" "curl -LJO 'https://github.com/fwcd/kotlin-language-server/releases/download/1.3.7/server.zip'"
 		unzip server.zip
 		rm -rf ~/lsp
 		mkdir -p ~/lsp/kotlin
@@ -88,8 +88,8 @@ main () {
 
 	(
 		#git
-		git clone https://www.github.com/diamond2sword/termux-fun
-		git-bash clone project	
+		force_move_file_with_cmd d "$HOME/termux-fun" "git clone 'https://www.github.com/diamond2sword/termux-fun'"
+		git_bash_clone project	
 		apt install openssh
 
 		#gradle needs internet
@@ -99,7 +99,7 @@ main () {
 
 	yes | {
 		#termux JetBrainsMono font
-		curl -LJO https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip
+		force_move_file_with_cmd f "JetBrainsMono-2.304.zip" "curl -LJO 'https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip'"
 		unzip ~/JetBrainsMono-2.304.zip -d ~/font
 		cp ~/font/fonts/ttf/JetBrainsMono-Regular.ttf ~/.termux/font.ttf
 		rm JetBrainsMono-2.304.zip
@@ -111,8 +111,10 @@ main () {
 		#zsh
 		apt install zsh
 
-		sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"	
-		git clone --depth=1 https://github.com/mattmc3/antidote.git "${ZDOTDIR:-$HOME}/.antidote"
+		force_move_file_with_cmd f "$HOME/install.sh" "curl -LJO 'https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh'"
+		sh -c "$(cat "$HOME/install.sh")"
+		local antidote_path="${ZDOTDIR:-$HOME}/.antidote"
+		force_move_file_with_cmd d "git clone --depth=1 'https://github.com/mattmc3/antidote.git' '$antidote_path'"
 
 		#because antidote has to install plugins for zsh
 		chsh -s zsh
@@ -124,20 +126,20 @@ main () {
 
 	yes | {
 		#study
-		git-bash clone study
-		git-bash clone bash-fun
-		git-bash clone java-fun
+		git_bash_clone study
+		git_bash_clone bash-fun
+		git_bash_clone java-fun
 
 		#coding stuffs
 		{
 			# c++, c
-			git-bash clone cpp-fun
-			git-bash clone c-fun
+			git_bash_clone cpp-fun
+			git_bash_clone c-fun
 			apt install clang
 		}
 		{
 			# assembly
-			git-bash clone asm-fun
+			git_bash_clone asm-fun
 			apt install binutils
 		}
 		{
@@ -146,97 +148,103 @@ main () {
 		}
 		{
 			# python
-			git-bash clone python-fun
+			git_bash_clone python-fun
 			apt install python
 		}
 		{
 			# print
-			git-bash clone print-shop
+			git_bash_clone print-shop
 			apt install file shfmt
 			apt install imagemagick pandoc pdf2svg pdftk qpdf
 		}
 	}
 }
 
-git-bash () {
-	bash $HOME/termux-fun/git.bash "$@"
+git_bash_clone () {
+	local repo_name="$1"
+	local dst_file="$HOME/$repo_name"
+	local cmd="bash '$HOME/termux-fun/git.bash' clone '$repo_name'"
+	force_move_file_with_cmd d "$dst_file" "$cmd"
 }
 
-do_coc_install () {
-COC_EXTENSION=$1 expect <<- "EOF"
+force_move_file_with_cmd () {
+	local file_type="$1"
+	local dst_path="$2"
+	local cmd="$3"
+	while true; do
+		if [ "$file_type" == "d" ]; then
+			if [ -d "$dst_path" ]; then
+				return
+			fi
+		fi
+		if [ "$file_type" == "f" ]; then
+			if [ -f "$dst_path" ]; then
+				return
+			fi
+		fi
+		echo "force_move_file_with_cmd: executing: $cmd"
+		eval "$cmd"
+	done
+}
+
+force_coc_install () {
+	local coc_extension="$1"
+	while (! is_coc_extension_installed "$coc_extension"); do
+		coc_install "$coc_extension"
+	done
+}
+
+coc_install () {
+COC_EXTENSION="$1" \
+expect <<- "EOF"
 	set timeout -1
 	set cocExtension $env(COC_EXTENSION)
 	spawn nvim
 	send ":CocInstall $cocExtension\r"
-	expect_before -re "Installi" {
-		send ":wincmd w|q\r"
-		expect_before -re "Move|e ext|finished" {
-			send ":q\r"
+	expect {
+		-re "Installi" {
+			send ":wincmd w|q\r"
+			expect {
+				-re "Move|e ext|finished" {
+					exit 0
+				}
+				-re "Error" {
+					exit 1
+				}
+				eof
+			}
 		}
-		expect eof
+		eof
 	}
-	expect eof
 EOF
 }
 
-INIT_LUA=$(cat << "LUAEOF"
-
-LUAEOF
-)
-
-INIT_VIM=$(cat << "EOF"
-call plug#begin('~/.config/nvim/plugged')
-Plug 'morhetz/gruvbox' "https://github.com/neoclide/coc.nvim/issues/3784
-Plug 'sheerun/vim-polyglot'
-Plug 'frazrepo/vim-rainbow'
-Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
-Plug 'junegunn/fzf.vim'
-Plug 'neoclide/coc.nvim', {'branch': 'release'}
-Plug 'josa42/vim-lightline-coc'
-Plug 'itchyny/lightline.vim'
-call plug#end()
-
-"https://linuxhandbook.com/vim-indentation-tab-spaces/
-set autoindent
-set noexpandtab
-set tabstop=4 
-set shiftwidth=4
-
-"https://www.reddit.com/r/neovim/comments/131urrq/make_the_cursorline_in_neovim_semitransparent/
-nmap <C-g> :Files<CR>
-
-"https://github.com/neoclide/coc.nvim/issues/3784
-set t_Co=256
-syntax enable
-silent! colorscheme gruvbox
-autocmd ColorScheme * highlight CocHighlightText gui=None guibg=#665c54
-set background=dark
-let g:coc_default_semantic_highlight_groups = 1
-
-"https://jeffkreeftmeijer.com/vim-number/
-set number
-augroup numbertoggle
-	autocmd!
-	autocmd BufEnter,FocusGained,InsertLeave,WinEnter * if &nu && mode() != "i" | set rnu   | endif
-	autocmd BufLeave,FocusLost,InsertEnter,WinLeave   * if &nu                  | set nornu | endif
-augroup END
-
-"https://www.reddit.com/r/vim/comments/eck1tl/comment/fbcox26/
-set cursorline
-
-"https://github.com/neoclide/coc.nvim#example-vim-configuration
-verbose imap <tab>
-
-let g:lightline = {
-  \   'active': {
-  \     'left': [[  'coc_info', 'coc_hints', 'coc_errors', 'coc_warnings', 'coc_ok' ], [ 'coc_status'  ]]
-  \   }
-  \ }
-" register components:
-call lightline#coc#register()
-
+is_coc_extension_installed () {
+COC_EXTENSION="$1" \
+COC_EXTENSION_CLUE="$(echo "$1" | remove_hyphen)" \
+expect <<- "EOF"
+	set timeout -1
+	set cocExtension $env(COC_EXTENSION)
+	set cocExtensionClue $env(COC_EXTENSION_CLUE)
+	spawn nvim
+	send ":CocList --tab extensions\r"
+	expect {
+		-re "FUZZY" {
+			send "$cocExtensionClue"
+			expect {
+				-re "$cocExtension" {exit 0} 
+				-re "No results" 	{exit 1} 
+				eof
+			}
+		}
+		eof
+	}
 EOF
-)
+}
+
+remove_hyphen () {
+	sed "s/-//g"
+}
 
 INIT_VIM=$(cat << "EOF"
 call plug#begin('~/.config/nvim/plugged')
@@ -516,150 +524,6 @@ COC_CONFIG=$(cat << "JSONEOF"
 JSONEOF
 )
 
-OPTIMIZE_INIT_GRADLE_KTS=$(cat << "KOTLINEOF"
-fun main() {
-}
-
-main()
-KOTLINEOF
-)
-
-OFFLINE_INIT_GRADLE_KTS=$(cat << "EOF"
-fun main() {
-	addLocalRepo()
-	configureCacheToRepoTask()
-}
-
-fun configureCacheToRepoTask() {
-	allprojects {
-		buildscript {
-			fun cacheToRepoInteractive() {
-				val mustSkipCacheToRepo: String? by project
-				val isVerboseCacheToRepo: String? by project
-				val mustSkip = mustSkipCacheToRepo?.toBooleanStrictOrNull()
-				val isVerbose = isVerboseCacheToRepo?.toBooleanStrictOrNull()
-				cacheToRepo(mustSkip, isVerbose)
-			}
-			taskGraph.whenReady {
-				val userSpecifiedTasks = startParameter.taskNames
-				val allTasks = taskGraph.getAllTasks()
-				if (userSpecifiedTasks.isNotEmpty()) {
-					val lastTask = allTasks.last()
-					lastTask.doLast {
-						cacheToRepoInteractive()	
-					}
-				}
-			}
-		}
-	}
-}
-
-fun addLocalRepo() {
-	val reposDir = gradle.getGradleUserHomeDir().resolve("repos")
-	val repoDir = reposDir.resolve("m2")
-	repoDir.mkdirs()
-	val repos = reposDir.listFiles().toList()
-	beforeSettings {
-		pluginManagement.repositories.addRepos(listOf(repoDir))
-	}
-	allprojects {
-		repositories.addRepos(repos)
-		buildscript.repositories.addRepos(repos)
-	}
-}
-
-fun RepositoryHandler.addRepos(repos: List<File>?) {
-	maven {
-		repos?.forEach { repo ->
-			setUrl(repo.toURI())
-		}
-	}
-	gradlePluginPortal()
-	mavenCentral()
-	google()
-}
-
-
-fun cacheToRepo(mustSkip: Boolean? = null, isVerboseParam: Boolean? = null) {
-	fun askUser(question: String) : Boolean {
-		println("$question (yes/no)")
-		return readLine()?.equals("yes", ignoreCase = true) ?: false
-	}
-	if (mustSkip ?: askUser("Skip cacheToRepo()?")) return
-
-	var isVerbose = isVerboseParam ?: false 
-	fun printVerbose(string: String) = when (isVerbose) {
-		true -> println(string)
-		false -> Unit
-	}
-	isVerbose = isVerboseParam ?: askUser("must be verbose?")
-
-	val excludedFiletypes = listOf(".module")
-	val includedFiletypes = listOf(".jar", ".pom")
-
-	val cacheDir = file("${gradle.gradleUserHomeDir}/caches/modules-2/files-2.1")
-	val customRepoDir = file("${gradle.gradleUserHomeDir}/repos/m2")
-
-	println("cacheToRepo task is called.")
-	println("cacheDir: $cacheDir")
-	println("customRepoDir: $customRepoDir")
-
-	cacheDir.walkTopDown().forEach { file ->
-		if (!file.isFile) {
-			printVerbose("File: ${file.name} - It's not a file.")
-			return@forEach
-		}
-		printVerbose("File: ${file.name} - It's a file.")
-		
-		excludedFiletypes.forEach loop2@ { filetype ->
-			if (!file.name.endsWith(filetype)) return@loop2
-			printVerbose("File: ${file.name} - Excluded Filetype: ${filetype}")
-			return@forEach
-		}
-		
-		var isFiletypeIncluded = false
-		for (filetype in includedFiletypes) {
-			if (!file.name.endsWith(filetype)) continue
-			isFiletypeIncluded = true
-			break
-		}
-		if (!isFiletypeIncluded) {
-			printVerbose("File: ${file.name} - Not in Included Filetypes: ${includedFiletypes.toString()}")
-			if (!askUser("Do you want to copy this file?")) return@forEach
-		}
-
-		val relativePath = file.relativeTo(cacheDir).path
-		val pathComponents = relativePath.split('/')
-		val longPath = pathComponents[0].replace(".", "/")
-		val name = pathComponents[1]
-		val version = pathComponents[2]
-
-		printVerbose("\tRelative path: $relativePath")
-		printVerbose("\tlongPath: $longPath")
-		printVerbose("\tname: $name")
-		printVerbose("\tversion: $version")
-		while (true) {	
-			try {
-				copy {
-					from(file)
-					into(customRepoDir.toPath().resolve("$longPath/$name/$version"))
-				}
-				printVerbose("Successfully copied ${file.name}.")
-				break
-			} catch (e: Exception) {
-				printVerbose("Failed to copy ${file.name}. Reason: ${e.message}")
-				if (askUser("Skip copying this one?")) break
-			}
-		}
-	}
-}
-
-main()
-EOF
-)
-
 main "$@"
 
-while true; do
-	exit
-done
+exit
